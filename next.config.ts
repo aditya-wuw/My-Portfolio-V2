e1c51d3787d270d05948188857afeeac23277392
aditya-wuw <guddumandal605605@gmail.com>
2026-08-30 14:25:50 +0530

    remove ignoreBuildErrors

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
-    ignoreBuildErrors: true, // testing and migration only remove later
+    // ignoreBuildErrors: true, // testing and migration only remove later
   },
   images: {
     remotePatterns: [
